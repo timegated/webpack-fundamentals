98d0abce909133d7e6cf81ea0143fe6cc5439353
dbeccaria826 <beccdaniel@gmail.com>
2020-04-11 13:01:26 -0400

    using env with webpack config and presets

--- 1-webpack/webpack.config.js
+++ 1-webpack/webpack.config.js
@@ -1,12 +1,18 @@
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpackMerge = require('webpack-merge');
 
-module.exports = env => {
-    console.log(env)
-    return {
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const presetConfig = require("./build-utils/loadPresets");
+module.exports = ({ mode, presets } = {mode: "production", presets: []})  => {
+    return webpackMerge({
+        mode,
         output: {
             filename: 'bundle.js'
         },
         plugins: [new HtmlWebPackPlugin(), new webpack.ProgressPlugin()]
-    };
+    },
+        modeConfig(mode),
+        presetConfig({mode, presets})
+    );
 };

583e47e20ff39b4146818052d620fb402809cc3c
dbeccaria826 <beccdaniel@gmail.com>
2020-04-11 13:11:58 -0400

    fixed problem with undefined env

--- 1-webpack/webpack.config.js
+++ 1-webpack/webpack.config.js
@@ -2,8 +2,9 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 
-const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
-const presetConfig = require("./build-utils/loadPresets");
+const modeConfig = env => require(`./build-utils/webpack.${env}.js`)(env);
+
+const presetConfig = require('./build-utils/loadPresets');
 module.exports = ({ mode, presets } = {mode: "production", presets: []})  => {
     return webpackMerge({
         mode,

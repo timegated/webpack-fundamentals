67085c764d07c1d9396042ed2e6f6d54d868a5e1
dbeccaria826 <beccdaniel@gmail.com>
2020-04-20 11:56:53 -0400

    modified index.html and removed footer styling

--- 1-webpack/build-utils/webpack.development.js
+++ 1-webpack/build-utils/webpack.development.js
@@ -2,6 +2,8 @@ module.exports = () => ({
     module: {
         rules: [{
             test: /\.css$/, use: ["style-loader","css-loader"]
+        }, {
+            test: /\.html$/, use: ["html-loader"]
         }]
     }
 })
